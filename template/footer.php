bb5402f3acead976158d44be65e763194f646b6f
Khazza <mahetoquet@gmail.com>
2023-06-13 09:18:03 +0200

    edit

--- template/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer text-center">
-    <a href="#"><img src="path/to/facebook-icon.png" alt="Facebook"></a>
-    <a href="#"><img src="path/to/twitter-icon.png" alt="Twitter"></a>
-    <a href="#"><img src="path/to/instagram-icon.png" alt="Instagram"></a>
-</footer>
