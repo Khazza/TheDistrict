828ef4deff2bbeba602e34a9febce3cf6f4e5936
Khazza <mahetoquet@gmail.com>
2023-06-13 09:55:53 +0200

    edit

--- template/functions.php
+++ template/functions.php
@@ -3,7 +3,7 @@
 function render_header() {
     ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand"><img src="../src/img/the_district_brand/logo.png" alt="Logo" height="60"></a>
+        <a class="navbar-brand"><img src="../src/img/the_district_brand/logo.png" alt="Logo"></a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
@@ -19,10 +19,16 @@ function render_header() {
                     <a class="nav-link" href="contact.php">Contact</a>
                 </li>
             </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link login-button">S'identifier</a>
+                </li>
+            </ul>
         </div>
     </nav>
     <?php
 }
+
 
 function render_footer() {
     ?>
