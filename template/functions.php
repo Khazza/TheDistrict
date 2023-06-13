d6a44ee7264cd7aae21ab26f55d6631c5429afb9
Khazza <mahetoquet@gmail.com>
2023-06-13 10:03:38 +0200

    edit

--- template/functions.php
+++ template/functions.php
@@ -3,7 +3,7 @@
 function render_header() {
     ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand"><img src="../src/img/the_district_brand/logo.png" alt="Logo"></a>
+        <a class="navbar-brand"><img src="../src/img/the_district_brand/logo.png" alt="Logo" height="60"></a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
