7e4b12c768d719ec0c7288ec0bf2d55c0c40de97
Khazza <mahetoquet@gmail.com>
2023-06-19 08:43:49 +0200

    edit

--- legal-notice.php
+++ legal-notice.php
@@ -3,35 +3,41 @@ include 'database.php';
 include 'DAO.php';
 include './template/functions.php';
 ?>
+
 <!-- Appel de la fonction pour afficher le header -->
 <?php render_header(); ?>
 
+<div class="legal-notice">
+    <h1>Mentions légales</h1>
+    
+    <h2>Société éditrice du site</h2>
+    <p><strong>The District</strong></p>
+    <p>SAS au capital de 00.000 euros</p>
+    <p>N° TVA : FR XX XXXXXXXXX</p>
+    <p>Code NAF : 4791A</p>
+    <p>Siège social :</p>
+    <address>
+        XXX avenue d'XXXXXX, <br>
+        XXXXX XXXXXXXXX
+    </address>
+    <p>Tél : -- -- -- -- --</p>
+    <p>Mail : <a href="mailto:reply@thedistrict.com">reply@thedistrict.com</a></p>
 
-Mentions légales
-Société éditrice du site
+    <h2>Directeur de publication</h2>
+    <p>Prenom NOM</p>
+    <p>Mail : <a href="mailto:reply@thedistrict.com">reply@thedistrict.com</a></p>
 
-The District
-SAS au capital de 00.000 euros
-N° TVA : FR XX XXXXXXXXX
-Code NAF : 4791A
-Siège social :
-XXX avenue d'XXXXXX,XXXXX XXXXXXXXX
-
-Tél : -- -- -- -- --
-Mail : reply@thedistrict.com
-
-Directeur de publication
-Prenom NOM
-Mail : reply@thedistrict.com
-
-Hébergeur du site
-OVH
-SAS au capital de 10.069.020 euros
-Siège social :
-2 rue Kellermann, 59100 Roubaix
-Tél : 1007
-Mail : contact@ovh.com
-
+    <h2>Hébergeur du site</h2>
+    <p><strong>OVH</strong></p>
+    <p>SAS au capital de 10.069.020 euros</p>
+    <p>Siège social :</p>
+    <address>
+        2 rue Kellermann, <br>
+        59100 Roubaix
+    </address>
+    <p>Tél : 1007</p>
+    <p>Mail : <a href="mailto:contact@ovh.com">contact@ovh.com</a></p>
+</div>
 
 <!-- Appel de la fonction pour afficher le footer -->
 <?php render_footer(); ?>
