7a228b3efa0f0913a8d6950dd3fcfe9fd9605e60
Khazza <mahetoquet@gmail.com>
2023-06-26 14:12:56 +0200

    edit

--- orders.php
+++ orders.php
@@ -16,46 +16,47 @@ if ($plat_id) {
 render_header();
 ?>
 
-<div class="container">
-    <!-- Carte du plat -->
-    <?php if ($plat) : ?>
-        <div class="plat-card2">
-            <img src="src/img/food/<?= $plat['image'] ?>" alt="<?= $plat['libelle'] ?>">
-            <div class="card-body">
-                <h2 class="card-title"><?= $plat['libelle'] ?></h2>
-                <p class="card-text"><?= $plat['description'] ?></p>
-                <label for="quantite">Quantité:</label>
-                <input type="number" id="quantite" name="quantite" min="1" max="25" value="1">
+<div class="content-wrapper">
+    <div class="container">
+        <!-- Carte du plat -->
+        <?php if ($plat) : ?>
+            <div class="plat-card2">
+                <img src="src/img/food/<?= $plat['image'] ?>" alt="<?= $plat['libelle'] ?>">
+                <div class="card-body">
+                    <h2 class="card-title"><?= $plat['libelle'] ?></h2>
+                    <p class="card-text"><?= $plat['description'] ?></p>
+                    <label for="quantite">Quantité:</label>
+                    <input type="number" id="quantite" name="quantite" min="1" max="25" value="1">
+                </div>
             </div>
-        </div>
-    <?php endif; ?>
+        <?php endif; ?>
 
 
-    <!-- Formulaire de commande -->
-    <form action="submit_order.php" method="post" class="order-form">
-        <div class="form-group">
-            <label for="nom_prenom">Nom et prénom *</label>
-            <input type="text" id="nom_prenom" name="nom_prenom" required>
-        </div>
+        <!-- Formulaire de commande -->
+        <form action="submit_order.php" method="post" class="order-form">
+            <div class="form-group">
+                <label for="nom_prenom">Nom et prénom *</label>
+                <input type="text" id="nom_prenom" name="nom_prenom" required>
+            </div>
 
-        <div class="form-group">
-            <label for="email">Email *</label>
-            <input type="email" id="email" name="email" required>
-        </div>
+            <div class="form-group">
+                <label for="email">Email *</label>
+                <input type="email" id="email" name="email" required>
+            </div>
 
-        <div class="form-group">
-            <label for="telephone">Téléphone *</label>
-            <input type="tel" id="telephone" name="telephone" required>
-        </div>
+            <div class="form-group">
+                <label for="telephone">Téléphone *</label>
+                <input type="tel" id="telephone" name="telephone" required>
+            </div>
 
-        <div class="form-group">
-            <label for="adresse">Adresse</label>
-            <textarea id="adresse" name="adresse"></textarea>
-        </div>
+            <div class="form-group">
+                <label for="adresse">Adresse</label>
+                <textarea id="adresse" name="adresse"></textarea>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Commander</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Commander</button>
+        </form>
+    </div>
 </div>
-
 <!-- Appel de la fonction pour afficher le footer -->
 <?php render_footer(); ?>
