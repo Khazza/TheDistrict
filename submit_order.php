45a05fc5ee992f4c27daf51c871ae9d94f720917
Khazza <mahetoquet@gmail.com>
2023-06-27 16:03:01 +0200

    edit

--- submit_order.php
+++ submit_order.php
@@ -6,9 +6,6 @@ require_once 'database.php';
 require_once 'DAO.php';
 require_once 'vendor/autoload.php';
 
-$database = new Database();
-$db = $database->getConnection();
-
 // Récupération des données du formulaire
 $plat_id = $_POST['id_plat'];
 $quantite = $_POST['quantite'];
@@ -17,17 +14,29 @@ $email_client = $_POST['email'];
 $telephone_client = $_POST['telephone'];
 $adresse_client = $_POST['adresse'];
 
-// Récupération du prix du plat en utilisant la fonction dans DAO.php
+// Créer une connexion à la base de données
+$database = new Database();
+$db = $database->getConnection();
+
+// Récupération du prix du plat
 $prix = get_plat_prix($db, $plat_id);
-if ($prix === null) {
-    die("Erreur lors de la récupération du prix du plat.");
-}
 
 // Calcul du total
 $total = $prix * $quantite;
 
-// Insertion dans la base de données en utilisant la fonction dans DAO.php
+// Insertion dans la base de données
 insert_order($db, $plat_id, $quantite, $total, $nom_client, $telephone_client, $email_client, $adresse_client);
+
+// Construire le résumé de la commande
+$order_summary = "Résumé de votre commande: \n";
+$order_summary .= "Nom: $nom_client \n";
+$order_summary .= "Email: $email_client \n";
+$order_summary .= "Téléphone: $telephone_client \n";
+$order_summary .= "Adresse: $adresse_client \n";
+$order_summary .= "ID du plat: $plat_id \n";
+$order_summary .= "Quantité: $quantite \n";
+$order_summary .= "Prix unitaire: $prix \n";
+$order_summary .= "Total: $total \n";
 
 // Configuration de PHPMailer
 $mail = new PHPMailer(true);
@@ -40,9 +49,9 @@ $mail->Port = 1025;
 $mail->setFrom('from@thedistrict.com', 'The District Company');
 $mail->addAddress($email_client, $nom_client);
 
-$mail->isHTML(true);
+$mail->isHTML(true); // Si vous voulez utiliser du HTML à la place du texte brut, changez ceci en true
 $mail->Subject = 'Confirmation de commande';
-$mail->Body    = 'Merci pour votre commande! Votre commande est en attente et sera traitée prochainement.';
+$mail->Body    = $order_summary;
 
 try {
     $mail->send();
@@ -51,5 +60,7 @@ try {
     echo "L'envoi de mail a échoué. L'erreur suivante s'est produite : ", $mail->ErrorInfo;
 }
 
-// header('Location: orders.php'); // Rediriger vers la page de commande
+// Rediriger vers la page de commande pour le plat spécifique
+header("Location: orders.php?id=$plat_id");
+exit;
 ?>
