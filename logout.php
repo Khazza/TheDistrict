9b24263821ee887e942f7541a7654f5020718a6b
Khazza <mahetoquet@gmail.com>
2023-06-28 12:11:12 +0200

    edit

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+// Supprime toutes les variables de session
+session_unset();
+
+// Détruit la session
+session_destroy();
+
+// Redirige vers la page d'accueil avec le paramètre de requête logout=success
+header('Location: index.php');
+exit();
+?>
