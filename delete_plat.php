455e8c8388c8d1dc1f82db3b67f3cce7dc4fff5e
Khazza <mahetoquet@gmail.com>
2023-06-29 15:54:21 +0200

    edit

--- delete_plat.php
+++ delete_plat.php
@@ -6,7 +6,15 @@ include 'DAO.php';
 $database = new Database();
 $db = $database->getConnection();
 
-$id = $_POST['id'];
+// Utilisez $_GET au lieu de $_POST pour récupérer l'ID à partir de l'URL
+$id = isset($_GET['id']) ? $_GET['id'] : null;
 
-deletePlat($db, $id);
+if ($id) {
+    deletePlat($db, $id);
+    // Rediriger vers la page admin_dashboard.php après la suppression
+    header('Location: admin_dashboard.php');
+} else {
+    // Gérer le cas où l'ID n'est pas défini
+    echo "ID non défini";
+}
 ?>
