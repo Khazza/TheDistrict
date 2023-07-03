ad6908673613b0ae4c99d17461421dfc02a5b24d
Khazza <mahetoquet@gmail.com>
2023-07-03 14:02:25 +0200

    toukassé

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -11,7 +11,6 @@ if (!isset($_SESSION['user']['nom_prenom']) || $_SESSION['user']['role'] !== 'ad
     exit();
 }
 
-
 // Appel de la fonction pour afficher le header
 render_header();
 
@@ -20,142 +19,147 @@ $categories = get_all_categories();
 $plats = get_all_plats();
 ?>
 
-<div class="container dashboard">
+<div class="container dashboard my-5">
 
     <!-- Section de gestion des catégories -->
-    <h2>Gestion des catégories</h2>
+    <div class="card mb-5">
+        <div class="card-header">
+            <h2>Gestion des catégories</h2>
+        </div>
+        <div class="card-body">
+            
+            <!-- Ajout de la section pour ajouter une nouvelle catégorie -->
+            <h3 class="mb-3">Ajouter une nouvelle catégorie</h3>
+            <form action="add_category.php" method="post" enctype="multipart/form-data" class="mb-4">
+                <div class="form-group">
+                    <label for="libelle">Libelle:</label>
+                    <input type="text" name="libelle" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="image">Image:</label>
+                    <input type="file" name="image" class="form-control-file" required>
+                </div>
+                <div class="form-group">
+                    <label for="active">Active:</label>
+                    <select name="active" class="form-control">
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                    </select>
+                </div>
+                <input type="submit" value="Ajouter" class="btn btn-primary">
+            </form>
 
-    <!-- Ajout de la section pour ajouter une nouvelle catégorie -->
-    <h3>Ajouter une nouvelle catégorie</h3>
-    <form action="add_category.php" method="post" enctype="multipart/form-data">
-        <label for="libelle">Libelle:</label>
-        <input type="text" name="libelle" required>
-        <label for="image">Image:</label>
-        <input type="file" name="image" required>
-        <label for="active">Active:</label>
-        <select name="active">
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-        </select>
-        <input type="submit" value="Ajouter">
-    </form>
-
-    <table class="dashboard-table">
-        <tr>
-            <th>ID</th>
-            <th>Libelle</th>
-            <th>Active</th>
-            <th>Image</th>
-            <th>Actions</th>
-        </tr>
-        <?php foreach ($categories as $categorie) : ?>
-            <tr>
-                <form action="update_category.php" method="post" enctype="multipart/form-data">
-                    <td><?php echo $categorie['id']; ?></td>
-                    <td><input type="text" name="libelle" value="<?php echo $categorie['libelle']; ?>"></td>
-                    <td>
-                        <select name="active">
-                            <option value="Yes" <?php if ($categorie['active'] === 'Yes') echo 'selected'; ?>>Yes</option>
-                            <option value="No" <?php if ($categorie['active'] === 'No') echo 'selected'; ?>>No</option>
-                        </select>
-                    </td>
-                    <td>
-                        <label for="image">Image:</label>
-                        <input type="file" name="image">
-                    </td>
-                    <td>
-                        <input type="hidden" name="id" value="<?php echo $categorie['id']; ?>">
-                        <input type="submit" value="Modifier">
-                    </td>
-                </form>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+            <!-- Tableau des catégories existantes -->
+            <table class="table dashboard-table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Libelle</th>
+                        <th>Active</th>
+                        <th>Image</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($categories as $categorie) : ?>
+                        <tr>
+                            <form action="update_category.php" method="post" enctype="multipart/form-data">
+                                <td><?php echo $categorie['id']; ?></td>
+                                <td><input type="text" name="libelle" class="form-control" value="<?php echo $categorie['libelle']; ?>"></td>
+                                <td>
+                                    <select name="active" class="form-control">
+                                        <option value="Yes" <?php if ($categorie['active'] === 'Yes') echo 'selected'; ?>>Yes</option>
+                                        <option value="No" <?php if ($categorie['active'] === 'No') echo 'selected'; ?>>No</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <label for="image">Image:</label>
+                                    <input type="file" name="image" class="form-control-file">
+                                </td>
+                                <td>
+                                    <input type="hidden" name="id" value="<?php echo $categorie['id']; ?>">
+                                    <input type="submit" value="Modifier" class="btn btn-secondary">
+                                </td>
+                            </form>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <!-- Section de gestion des plats -->
-    <h2>Gestion des plats</h2>
+    <div class="card">
+        <div class="card-header">
+            <h2>Gestion des plats</h2>
+        </div>
+        <div class="card-body">
+            <!-- Ajout de la section pour ajouter un nouveau plat -->
+            <h3 class="mb-3">Ajouter un nouveau plat</h3>
+            <form action="add_plat.php" method="post" enctype="multipart/form-data" class="mb-4">
+                <!-- ... Reste de votre formulaire pour ajouter un plat ... -->
+            </form>
 
-    <!-- Ajout de la section pour ajouter un nouveau plat -->
-    <h3>Ajouter un nouveau plat</h3>
-    <form action="add_plat.php" method="post" enctype="multipart/form-data">
-        <label for="libelle">Libelle:</label>
-        <input type="text" name="libelle" required>
-        <label for="description">Description:</label>
-        <input type="text" name="description" required>
-        <label for="prix">Prix:</label>
-        <input type="number" step="0.01" name="prix" required>
-        <label for="image">Image:</label>
-        <input type="file" name="image" required>
-        <label for="active">Active:</label>
-        <select name="active">
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-        </select>
-        <label for="id_categorie">Catégorie:</label>
-        <select name="id_categorie">
-            <?php foreach ($categories as $categorie) : ?>
-                <option value="<?php echo $categorie['id']; ?>"><?php echo $categorie['libelle']; ?></option>
-            <?php endforeach; ?>
-        </select>
-        <input type="submit" value="Ajouter">
-    </form>
-
-    <?php
-    // Regrouper les plats par catégorie
-    $plats_by_category = [];
-    foreach ($plats as $plat) {
-        $plats_by_category[$plat['id_categorie']][] = $plat;
-    }
-
-    // Itérer sur les catégories
-    foreach ($categories as $categorie) {
-        if (isset($plats_by_category[$categorie['id']])) {
-            echo "<h3 class='category-title'>{$categorie['libelle']}</h3>";
-
-            echo "<table class='dashboard-table'>
-                <tr>
-                    <th>ID</th>
-                    <th>Libelle</th>
-                    <th>Description</th>
-                    <th>Prix</th>
-                    <th>Active</th>
-                    <th>Image</th>
-                    <th>Actions</th>
-                </tr>";
-
-            // Itérer sur les plats de cette catégorie
-            foreach ($plats_by_category[$categorie['id']] as $plat) {
-                echo "<tr>
-                    <form action='update_plat.php' method='post' enctype='multipart/form-data'>
-                        <td>{$plat['id']}</td>
-                        <td><input type='text' name='libelle' value='{$plat['libelle']}'></td>
-                        <td><input type='text' name='description' value='{$plat['description']}'></td>
-                        <td><input type='text' name='prix' value='{$plat['prix']}'></td>
-                        <td>
-                            <select name='active'>
-                                <option value='Yes'" . ($plat['active'] === 'Yes' ? ' selected' : '') . ">Yes</option>
-                                <option value='No'" . ($plat['active'] === 'No' ? ' selected' : '') . ">No</option>
-                            </select>
-                        </td>
-                        <td>
-                            <label for='image'>Image:</label>
-                            <input type='file' name='image'>
-                        </td>
-                        <td>
-                            <input type='hidden' name='id' value='{$plat['id']}'>
-                            <input type='hidden' name='id_categorie' value='{$plat['id_categorie']}'>
-                            <input type='submit' value='Modifier'>
-                            <a href='delete_plat.php?id={$plat['id']}' onclick=\"return confirm('Êtes-vous sûr de vouloir supprimer ce plat?');\">Supprimer</a>
-                        </td>
-                    </form>
-                </tr>";
+            <?php
+            // Regrouper les plats par catégorie
+            $plats_by_category = [];
+            foreach ($plats as $plat) {
+                $plats_by_category[$plat['id_categorie']][] = $plat;
             }
 
-            echo "</table>";
-        }
-    }
-    ?>
+            // Itérer sur les catégories
+            foreach ($categories as $categorie) {
+                if (isset($plats_by_category[$categorie['id']])) {
+                    echo "<h3 class='category-title'>{$categorie['libelle']}</h3>";
 
+                    echo "<table class='table dashboard-table'>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Libelle</th>
+                                <th>Description</th>
+                                <th>Prix</th>
+                                <th>Active</th>
+                                <th>Image</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>";
+
+                    // Itérer sur les plats de cette catégorie
+                    foreach ($plats_by_category[$categorie['id']] as $plat) {
+                        echo "<tr>
+                            <form action='update_plat.php' method='post' enctype='multipart/form-data'>
+                                <td>{$plat['id']}</td>
+                                <td><input type='text' name='libelle' class='form-control' value='{$plat['libelle']}'></td>
+                                <td><input type='text' name='description' class='form-control' value='{$plat['description']}'></td>
+                                <td><input type='text' name='prix' class='form-control' value='{$plat['prix']}'></td>
+                                <td>
+                                    <select name='active' class='form-control'>
+                                        <option value='Yes'" . ($plat['active'] === 'Yes' ? ' selected' : '') . ">Yes</option>
+                                        <option value='No'" . ($plat['active'] === 'No' ? ' selected' : '') . ">No</option>
+                                    </select>
+                                </td>
+                                <td>
+                                    <label for='image'>Image:</label>
+                                    <input type='file' name='image' class='form-control-file'>
+                                </td>
+                                <td>
+                                    <input type='hidden' name='id' value='{$plat['id']}'>
+                                    <input type='hidden' name='id_categorie' value='{$plat['id_categorie']}'>
+                                    <input type='submit' value='Modifier' class='btn btn-secondary'>
+                                    <a href='delete_plat.php?id={$plat['id']}' class='btn btn-danger' onclick=\"return confirm('Êtes-vous sûr de vouloir supprimer ce plat?');\">Supprimer</a>
+                                </td>
+                            </form>
+                        </tr>";
+                    }
+
+                    echo "</tbody></table>";
+                }
+            }
+            ?>
+        </div>
+    </div>
 </div>
 
 <?php render_footer(); ?>
