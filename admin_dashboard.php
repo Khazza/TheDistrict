e5c067f536abca0898fc895fa9680862a1d61b66
Khazza <mahetoquet@gmail.com>
2023-07-04 09:57:56 +0200

    edit

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -266,9 +266,9 @@ $plats = get_all_plats();
                                     <td><?php echo $order['date_commande']; ?></td>
                                     <td>
                                         <select name="etat" class="form-control">
-                                            <option value="Livree" <?php if ($order['etat'] === 'Livree') echo 'selected'; ?>>Livré</option>
+                                            <option value="Livree" <?php if ($order['etat'] === 'Livree') echo 'selected'; ?>>Livrée</option>
                                             <option value="En cours de livraison" <?php if ($order['etat'] === 'En cours de livraison') echo 'selected'; ?>>En cours de livraison</option>
-                                            <option value="Annulee" <?php if ($order['etat'] === 'Annulee') echo 'selected'; ?>>Annulé</option>
+                                            <option value="Annulee" <?php if ($order['etat'] === 'Annulee') echo 'selected'; ?>>Annulée</option>
                                         </select>
                                     </td>
                                     <td><?php echo $order['nom_client']; ?></td>
