9632ffabc5ef11af6c7c447a8edbe4d6321cf52f
Khazza <mahetoquet@gmail.com>
2023-07-05 13:59:03 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ document.querySelector('.order-form').addEventListener('submit', function(e) {
         }
     });
 });
+
+$(document).ready(function() {
+    // code pour gérer l'affichage du modal pour modifier les catégories
+    window.showEditModal = function(categorie) {
+        // Remplir le formulaire avec les informations de la catégorie
+        $('#edit-category-id').val(categorie.id);
+        $('#edit-category-libelle').val(categorie.libelle);
+        // ... assigner les valeurs aux autres champs ...
+
+        // Afficher le modal
+        $('#editCategoryModal').modal('show');
+    };
+});
