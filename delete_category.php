62f4e6b0807bf14424bbf2988d6dbc73e2604531
Khazza <mahetoquet@gmail.com>
2023-07-05 14:20:53 +0200

    edit

--- delete_category.php
+++ delete_category.php
@@ -15,13 +15,36 @@ if (isset($_GET['id'])) {
     $database = new Database();
     $db = $database->getConnection();
 
-    // Supprimer la catégorie
-    $query = "DELETE FROM categorie WHERE id = :id";
-    $stmt = $db->prepare($query);
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
+    // Vérifier si la catégorie contient des plats
+    $check_plats_query = "SELECT COUNT(*) as count FROM plat WHERE id_categorie = :id";
+    $check_plats_stmt = $db->prepare($check_plats_query);
+    $check_plats_stmt->bindParam(':id', $id);
+    $check_plats_stmt->execute();
+    $result_plats = $check_plats_stmt->fetch(PDO::FETCH_ASSOC);
 
-    $_SESSION['deletion_message'] = "L'élément a été supprimé avec succès!";
+    if ($result_plats['count'] > 0) {
+        // La catégorie contient des plats, ne peut pas être supprimée
+        $_SESSION['deletion_message'] = "La catégorie ne peut pas être supprimée car elle contient des plats.";
+    } else {
+        // Vérifier si un plat de cette catégorie est utilisé dans une commande
+        $check_query = "SELECT COUNT(*) as count FROM commande c JOIN plat p ON c.id_plat = p.id WHERE p.id_categorie = :id";
+        $check_stmt = $db->prepare($check_query);
+        $check_stmt->bindParam(':id', $id);
+        $check_stmt->execute();
+        $result = $check_stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result['count'] > 0) {
+            // Un plat de cette catégorie est utilisé dans une commande, ne peut pas être supprimée
+            $_SESSION['deletion_message'] = "La catégorie ne peut pas être supprimée car un plat de cette catégorie fait partie d'une commande.";
+        } else {
+            // Supprimer la catégorie
+            $query = "DELETE FROM categorie WHERE id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $_SESSION['deletion_message'] = "L'élément a été supprimé avec succès!";
+        }
+    }
 
     header('Location: admin_dashboard.php');
     exit();
--- delete_plat.php
+++ delete_plat.php
@@ -3,20 +3,38 @@ session_start();
 include 'database.php';
 include 'DAO.php';
 
-$database = new Database();
-$db = $database->getConnection();
+if (!isset($_SESSION['user']['nom_prenom']) || $_SESSION['user']['role'] !== 'admin') {
+    // Rediriger vers la page de connexion ou une page d'erreur
+    header('Location: login.php');
+    exit();
+}
 
-$id = isset($_GET['id']) ? $_GET['id'] : null;
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-if ($id) {
-    deletePlat($db, $id);
+    $database = new Database();
+    $db = $database->getConnection();
 
-    $_SESSION['deletion_message'] = "L'élément a été supprimé avec succès!";
+    // Vérifier si le plat est utilisé dans une commande
+    $check_query = "SELECT COUNT(*) as count FROM commande WHERE id_plat = :id";
+    $check_stmt = $db->prepare($check_query);
+    $check_stmt->bindParam(':id', $id);
+    $check_stmt->execute();
+    $result = $check_stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Rediriger vers la page admin_dashboard.php après la suppression
+    if ($result['count'] > 0) {
+        // Le plat est utilisé dans une commande, ne peut pas être supprimé
+        $_SESSION['deletion_message'] = "Le plat ne peut pas être supprimé car il fait partie d'une commande.";
+    } else {
+        // Supprimer le plat
+        $query = "DELETE FROM plat WHERE id = :id";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $_SESSION['deletion_message'] = "Le plat a été supprimé avec succès!";
+    }
+
     header('Location: admin_dashboard.php');
-} else {
-    // Gérer le cas où l'ID n'est pas défini
-    echo "ID non défini";
+    exit();
 }
 ?>
